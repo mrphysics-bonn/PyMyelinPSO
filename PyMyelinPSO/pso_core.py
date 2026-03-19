bac640ce56c51882c0449b0f22642fa5985eccca
Martin Kobe <martin.kobe@email.de>
2026-03-19 12:23:10 +0100

    update for license + code adjustments

--- PyMyelinPSO/pso_core.py
+++ PyMyelinPSO/pso_core.py
@@ -3,7 +3,7 @@
 #
 # Authors:
 #   Martin Kobe (Helmholtz Centre for Environmental Research - UFZ)
-#   Segolene Dega (Helmholtz Centre for Environmental Research - UFZ)
+#   Ségolène Dega (Helmholtz Centre for Environmental Research - UFZ)
 #   Hendrik Paasche (Helmholtz Centre for Environmental Research - UFZ)
 #
 # This file is part of PyMyelinPSO.
